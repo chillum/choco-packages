0aabec3ffc4b4c9d0ca9bf759cf9e1a65ab62307
Vasily Korytov <v.korytov@outlook.com>
2020-06-15 00:47:27 +0300

    yq 3.3.2

--- yq/tools/chocolateyinstall.ps1
+++ yq/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
     PackageName    = 'yq'
-    Url            = 'https://github.com/mikefarah/yq/releases/download/3.3.0/yq_windows_386.exe'
-    Checksum       = 'B563F69FDC8078F4F8FEB95907D868BEAF945EFBCEE8BA2A601A220A20BF491A'
+    Url            = 'https://github.com/mikefarah/yq/releases/download/3.3.2/yq_windows_386.exe'
+    Checksum       = 'A0EF29A7AEBFD818DE0EA20150DDED8D23847D7BF997061464843240A9E7C170'
     ChecksumType   = 'sha256'
-    Url64bit       = 'https://github.com/mikefarah/yq/releases/download/3.3.0/yq_windows_amd64.exe'
-    Checksum64     = '9137DD05FFE568EA8438411E426ED039CFBFB317339F335C778CB53919F28277'
+    Url64bit       = 'https://github.com/mikefarah/yq/releases/download/3.3.2/yq_windows_amd64.exe'
+    Checksum64     = 'B3ADE9A5BCBF13C7C7013EECDBC54183D0392FD6360F2D19DBBE8C6141C75FD2'
     ChecksumType64 = 'sha256'
     FileFullPath   = "$toolsDir\yq.exe"
 }
