f21df865241744600d605a5e3be38059c1a5242e
Vasily Korytov <v.korytov@outlook.com>
2020-08-11 17:21:54 +0300

    sops 3.6.0

--- sops/tools/chocolateyinstall.ps1
+++ sops/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   PackageName    = 'sops'
-  Url64bit       = 'https://github.com/mozilla/sops/releases/download/v3.5.0/sops-v3.5.0.exe'
-  Checksum64     = '326F00573D01979D231FF07CBFDADB5F353A87998BA4144EC7C72F53D377AF13'
+  Url64bit       = 'https://github.com/mozilla/sops/releases/download/v3.6.0/sops-v3.6.0.exe'
+  Checksum64     = 'E39C46C07DD649900852EED54B680DBA36825D6756DA180C23D176FB35F37C92'
   ChecksumType64 = 'sha256'
   FileFullPath   = "$toolsDir\sops.exe"
 }
