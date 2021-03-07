4172b7bc06efafaad70f471ec88a201964a65527
Vasily Korytov <v.korytov@outlook.com>
2021-03-07 20:50:54 +0200

    httpstress 6.5.3

--- httpstress/tools/chocolateyinstall.ps1
+++ httpstress/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $packageArgs = @{
     PackageName   = 'httpstress'
-    Url           = 'https://github.com/chillum/httpstress/releases/download/v6.5.2/win32.zip'
-    Checksum      = 'C9A1CF3AC54D90101398906C169736EC7283A84008AB57C22CEAFA46523345DF'
+    Url           = 'https://github.com/chillum/httpstress/releases/download/v6.5.3/win32.zip'
+    Checksum      = '97FD0A3D135754A88F485378CCC337E55D3806808457C340A219651E804AE13A'
     ChecksumType  = 'sha256'
     UnzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
