38b01eab5af867ff45718b3480079d8253fd6b12
Vasily Korytov <v.korytov@outlook.com>
2021-07-20 02:00:06 +0300

    kops 1.20.2

--- kubernetes-kops/tools/chocolateyinstall.ps1
+++ kubernetes-kops/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
     PackageName    = 'kubernetes-kops'
-    Url64bit       = 'https://github.com/kubernetes/kops/releases/download/v1.19.3/kops-windows-amd64'
-    Checksum64     = 'FA18E04DC428E8212273CA2CBAF2A4EAABEC4DD97CD1609F2868FFDD32DC8C32'
+    Url64bit       = 'https://github.com/kubernetes/kops/releases/download/v1.20.2/kops-windows-amd64'
+    Checksum64     = '2394EAFA7046361B4D9A5E72F91CCA10454AE2EB210F5B85AFC42B11EF3D09CD'
     ChecksumType64 = 'sha256'
     FileFullPath   = "$toolsDir\kops.exe"
 }
