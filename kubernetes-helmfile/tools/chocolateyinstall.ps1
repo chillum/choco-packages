642f2a428c66e9ff045d4edd34a40969c094f636
Vasily Korytov <v.korytov@outlook.com>
2022-05-26 18:37:41 +0300

    helmfile 0.144.0

--- kubernetes-helmfile/tools/chocolateyinstall.ps1
+++ kubernetes-helmfile/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
     PackageName    = 'kubernetes-helmfile'
-    Url            = 'https://github.com/roboll/helmfile/releases/download/v0.142.0/helmfile_windows_386.exe'
-    Checksum       = 'BC24147E86DA7DBC5932F4843DF378AD2DCD533465C12F73D382A28D36DE0745'
+    Url            = 'https://github.com/roboll/helmfile/releases/download/v0.144.0/helmfile_windows_386.exe'
+    Checksum       = '3B30897FB08B49E023B134119AAFD9D9777CCD5586B3B565805EC8FBD8CBEDF8'
     ChecksumType   = 'sha256'
-    Url64bit       = 'https://github.com/roboll/helmfile/releases/download/v0.142.0/helmfile_windows_amd64.exe'
-    Checksum64     = '87F8CE227E4F9E7AD127B73500903ABCFF89F8012E755F9C65EE43D2135C07A1'
+    Url64bit       = 'https://github.com/roboll/helmfile/releases/download/v0.144.0/helmfile_windows_amd64.exe'
+    Checksum64     = '97F0E617977C5518CC92D9982C894ECCAEB4C64FA9AE9577E606D2780E66F10B'
     ChecksumType64 = 'sha256'
     FileFullPath   = "$toolsDir\helmfile.exe"
 }
