323d90c0857e999c4af4cdb74dc759cf49bcbfeb
Vasily Korytov <v.korytov@outlook.com>
2022-07-05 22:09:12 +0300

    yq 4.25.3

--- yq/tools/chocolateyinstall.ps1
+++ yq/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
     PackageName    = 'yq'
-    Url            = 'https://github.com/mikefarah/yq/releases/download/v4.25.1/yq_windows_386.exe'
-    Checksum       = '39546D622539F31306588F23B030989DE321EAEA27144492D2F7CD3A2A66195E'
+    Url            = 'https://github.com/mikefarah/yq/releases/download/v4.25.3/yq_windows_386.exe'
+    Checksum       = '25DB6F917BBFC54AE3B322D4919A15F8B3255A7D4163E0E16B6DEFBFDB01DF05'
     ChecksumType   = 'sha256'
-    Url64bit       = 'https://github.com/mikefarah/yq/releases/download/v4.25.1/yq_windows_amd64.exe'
-    Checksum64     = 'F26BF0F977163524FE181A597CB27E1798D326A4E85AFDD2BA176FFD4C3579B6'
+    Url64bit       = 'https://github.com/mikefarah/yq/releases/download/v4.25.3/yq_windows_amd64.exe'
+    Checksum64     = 'EB6D4A8B249FEAB20E738CB7BBED0DFABAAE1D44464A86149691B90529F047C0'
     ChecksumType64 = 'sha256'
     FileFullPath   = "$toolsDir\yq.exe"
 }
