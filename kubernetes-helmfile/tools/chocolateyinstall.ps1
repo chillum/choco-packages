5512d411400ffd2dcf3105f78595b0b8abd14f63
Vasily Korytov <v.korytov@outlook.com>
2022-10-13 22:41:29 +0300

    helmfile 0.147.0

--- kubernetes-helmfile/tools/chocolateyinstall.ps1
+++ kubernetes-helmfile/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
     PackageName    = 'kubernetes-helmfile'
-    Url            = 'https://github.com/helmfile/helmfile/releases/download/v0.145.4/helmfile_0.145.4_windows_386.tar.gz'
-    Checksum       = '57769AD14507A21E7D79486735BBDA4BD9A8A3213D10326BE6E2D96E43F8F385'
+    Url            = 'https://github.com/helmfile/helmfile/releases/download/v0.147.0/helmfile_0.147.0_windows_386.tar.gz'
+    Checksum       = '7C641B3C9DD9356553A5C122580B02D9C495536AE0CA23F5E43754A5D09F6A1B'
     ChecksumType   = 'sha256'
-    Url64bit       = 'https://github.com/helmfile/helmfile/releases/download/v0.145.4/helmfile_0.145.4_windows_amd64.tar.gz'
-    Checksum64     = '737074494A73DDD8DFCABE2CC6622F491FEE63CF8382A3857CE6CBBDA50FD213'
+    Url64bit       = 'https://github.com/helmfile/helmfile/releases/download/v0.147.0/helmfile_0.147.0_windows_amd64.tar.gz'
+    Checksum64     = '17023A515EC91DC8394A72E942CD033AF57540EEFF47A13833ED95DAFD95AE37'
     ChecksumType64 = 'sha256'
     UnzipLocation = "$toolsDir"
 }
@@ -14,4 +14,4 @@ $packageArgs = @{
 Install-ChocolateyZipPackage @packageArgs
 $tarFile = Get-ChildItem -File -Path $toolsDir -Filter *.tar
 Get-ChocolateyUnzip -fileFullPath $tarFile.FullName -destination $toolsDir
-rm $tarFile.FullName
+Remove-Item $tarFile.FullName
