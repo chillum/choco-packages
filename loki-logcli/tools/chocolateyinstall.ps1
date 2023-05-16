0435f4421a57c1d5664613b4572a0c49a76c8353
Vasily Korytov <v.korytov@outlook.com>
2023-05-16 18:53:24 +0300

    logcli 2.8.2

--- loki-logcli/tools/chocolateyinstall.ps1
+++ loki-logcli/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
     PackageName    = 'loki-logcli'
-    Url64bit       = 'https://github.com/grafana/loki/releases/download/v2.6.1/logcli-windows-amd64.exe.zip'
-    Checksum64     = '5CCA9C2C498C50F07C6A5F6E9A8AA22AB9CFB480263F90F3C586B711B9964E0D'
+    Url64bit       = 'https://github.com/grafana/loki/releases/download/v2.8.2/logcli-windows-amd64.exe.zip'
+    Checksum64     = 'FA900E4D9B77BF5F349CEFDADADD4AD921D940DD58D4C74BEFCBA07A133BDF11'
     ChecksumType64 = 'sha256'
     UnzipLocation  = "$toolsDir"
 }
