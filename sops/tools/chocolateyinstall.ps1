dd104e88dfc2c0f814c03d80d61c9bb9165d23e4
Vasily Korytov <v.korytov@outlook.com>
2023-11-16 02:19:37 +0200

    sops 3.8.1

--- sops/tools/chocolateyinstall.ps1
+++ sops/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   PackageName    = 'sops'
-  Url64bit       = 'https://github.com/mozilla/sops/releases/download/v3.7.3/sops-v3.7.3.exe'
-  Checksum64     = '0CCDA78BC7F7DBF3F07167221F2A42CAB2B10D02DE7C26FE8E707EFAACAF3BD2'
+  Url64bit       = 'https://github.com/mozilla/sops/releases/download/v3.8.1/sops-v3.8.1.exe'
+  Checksum64     = 'FE1F6299294B47CEDA565E1091E843EE3F3DB58764901D4298EB00558189E25F'
   ChecksumType64 = 'sha256'
   FileFullPath   = "$toolsDir\sops.exe"
 }
